f90573441c51301b137c38fb8b4098dcde267b41
Mohammad Bahmani <bahmani.m.n@gmail.com>
2026-05-04 22:46:07 +0200

    make deployment script more user friendly

--- env_preparation/cleanup_root_application.sh
+++ env_preparation/cleanup_root_application.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
+BLUE='\033[0;36m'
 NC='\033[0m' # No Color
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
